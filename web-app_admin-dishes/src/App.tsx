b2eaacf3d2e58c42cd5e9747715be98db1793df3
Солтангазиева Мария Алексеевна <avemaryplus@gmail.com>
2023-08-01 11:51:08 +0600

    #1 Добавлена маршрутизация страниц и Provider, применены основные стили

--- web-app_admin-dishes/src/App.tsx
+++ web-app_admin-dishes/src/App.tsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import DishesPage from "./containers/DishesPage";
+import OrdersPage from "./containers/OrdersPage";
+import Layout from "./components/Layout/Layout";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<DishesPage />} />
+            <Route path="/dishes" element={<DishesPage />} />
+            <Route path="/orders" element={<OrdersPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
