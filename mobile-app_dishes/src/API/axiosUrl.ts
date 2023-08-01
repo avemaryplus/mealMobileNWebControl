dfee379f0b35ed381f262f9d073afc27df8cb53a
Солтангазиева Мария Алексеевна <avemaryplus@gmail.com>
2023-08-01 11:55:17 +0600

    #2 Добавлен базовый url для axios

---
+++ mobile-app_dishes/src/API/axiosUrl.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+
+const axiosUrl = axios.create({
+  baseURL:
+    "https://restaurant-menu-attractor-default-rtdb.europe-west1.firebasedatabase.app/",
+});
+
+export default axiosUrl;
