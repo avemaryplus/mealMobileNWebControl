c7f3b4fe109358c111cba24750a8c991bc932d22
Солтангазиева Мария Алексеевна <avemaryplus@gmail.com>
2023-08-01 12:02:45 +0600

    #2 подключен Provider и главный контейнер

--- mobile-app_dishes/App.tsx
+++ mobile-app_dishes/App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import store from "./src/app/store/store";
+import DishesPage from "./src/containers/Dishes/DishesPage";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <DishesPage />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
